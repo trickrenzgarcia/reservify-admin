4f151784e2c24b606df64cb9da519665e161dd0b
rwick <trickrenzgarcia@gmail.com>
2024-10-06 10:16:50 +0800

    refactor: enable to push .env.local

--- src/lib/test.ts
+++ src/lib/test.ts
@@ -1,12 +1,4 @@
 async function main() {
-  const { editUser } = await import("./firebase/service");
-
-  const testEdit = await editUser("90pkJldMdRzVtGjg0q4e", {
-    name: "Patrick",
-    email: "trickrenzgarcia@gmail.com",
-  });
-  console.log(testEdit);
-
   process.exit(0);
 }
 
