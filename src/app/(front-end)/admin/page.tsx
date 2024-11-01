023191573e92ef5b9044dee1abc3c49bb66cb480
rwick <trickrenzgarcia@gmail.com>
2024-11-01 20:40:06 +0800

    test

--- src/app/(front-end)/admin/page.tsx
+++ src/app/(front-end)/admin/page.tsx
@@ -2,23 +2,25 @@ import HomeHeader from '@/components/Interface/HomeHeader';
 import Sidebar from '@/components/Layout/Sidebar';
 import { Card, CardContent } from '@/components/ui/card';
 import { auth } from '@/lib/auth';
-import { getUsersCount } from '@/lib/firebase/service';
+import { getCollection, getUsersCount } from '@/lib/firebase/service';
+import { Reserve } from '@/lib/firebase/types';
 import { redirect } from 'next/navigation';
 
 export default async function Home() {
   const session = await auth();
 
-  if(!session?.user) {
-    redirect('/login');
-  }
+  // if(!session?.user) {
+  //   redirect('/login');
+  // }
 
   const usersCount = await getUsersCount();
+  const reservations = await getCollection<Reserve>("reservations")
 
   return (
     <div className="min-h-[calc(100vh-94px)] flex w-full">
       <Sidebar />
       <div className='flex-1 flex flex-col p-4 lg:p-16'>
-        <HomeHeader name={session.user.name} />
+        {session && <HomeHeader name={session.user.name} />}
         
         <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
           <Card className='border-4 border-black rounded-3xl'>
@@ -49,7 +51,7 @@ export default async function Home() {
             <CardContent className='py-8'>
               <div className='text-center'>
                 <h3 className='text-lg mb-4'>PENDING RESERVATIONS</h3>
-                <p className='text-4xl font-bold'>0</p>
+                <p className='text-4xl font-bold'>{reservations.length}</p>
               </div>
             </CardContent>
           </Card>
