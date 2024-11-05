afa96da1347de62779baf029ef90a4379a5eb0ef
rwick <trickrenzgarcia@gmail.com>
2024-11-06 05:21:14 +0800

    with apk

--- src/app/(front-end)/download/page.tsx
+++ src/app/(front-end)/download/page.tsx
@@ -1,7 +1,23 @@
+"use client"
+
+import Image from 'next/image'
 import React from 'react'
 
 export default function DownloadPage() {
+
+  function handleDownloadAPK() {
+    // Trigger download
+    const link = document.createElement('a');
+    link.href = '/Reservify.apk';
+    link.download = 'Reservify.apk';
+    link.click();
+  }
+
   return (
-    <div>DownloadPage</div>
+    <div className='w-full flex justify-center items-center my-16'>
+      <div className='cursor-pointer' onClick={handleDownloadAPK}>
+        <Image src='/Android_Download.png' alt='' width={1117} height={527} />
+      </div>
+    </div>
   )
 }
