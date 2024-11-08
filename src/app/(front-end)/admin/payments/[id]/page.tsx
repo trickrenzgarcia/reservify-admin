1cace207fd2491b20d5e4fb2aa1a775358b55f83
rwick <trickrenzgarcia@gmail.com>
2024-11-09 06:38:27 +0800

    w

--- src/app/(front-end)/admin/payments/[id]/page.tsx
+++ src/app/(front-end)/admin/payments/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Sidebar from '@/components/Layout/Sidebar';
 import { Suspense } from 'react';
 import PaymentData from './PaymentData';
+import { Loader2 } from 'lucide-react';
 
 type PageProps = {
   params: {
@@ -8,13 +9,21 @@ type PageProps = {
   }
 }
 
+function PageLoader() {
+  return (
+    <div className='w-full h-full flex items-center justify-center'>
+      <p>Loading... <Loader2  className='animate-spin' /></p>
+    </div>
+  )
+}
+
 export default function PaymentId({ params }: PageProps) {
 
   return (
     <div className="min-h-[calc(100vh-94px)] flex w-full">
       <Sidebar />
-      <div className='flex-1 flex flex-col p-0 lg:p-10'>
-        <Suspense fallback={<p>Loading...</p>}>
+      <div className='flex-1 flex flex-col px-1 py-4 lg:p-10'>
+        <Suspense fallback={<PageLoader />}>
           <PaymentData paymentId={params.id} />
         </Suspense>
       </div>
--- src/app/(front-end)/admin/payments/page.tsx
+++ src/app/(front-end)/admin/payments/page.tsx
@@ -3,6 +3,16 @@ import { auth } from '@/lib/auth';
 import { redirect } from 'next/navigation';
 import React, { Suspense } from 'react'
 import PaymentsTable from './components/PaymentsTable';
+import { Loader2 } from 'lucide-react';
+
+function PageLoader() {
+  return (
+    <div className='w-full h-full flex items-center justify-center'>
+      <p>Loading...</p>
+      <Loader2  className='animate-spin'/>
+    </div>
+  )
+}
 
 export default async function PaymentsPage() {
   const session = await auth();
@@ -14,8 +24,8 @@ export default async function PaymentsPage() {
   return (
     <div className="min-h-[calc(100vh-94px)] flex w-full">
       <Sidebar />
-      <div className='flex-1 flex flex-col p-0 lg:p-10'>
-        <Suspense fallback={<p>Loading...</p>}>
+      <div className='flex-1 flex flex-col px-1 py-4 lg:p-10'>
+        <Suspense fallback={<PageLoader />}>
           <PaymentsTable />
         </Suspense>
       </div>
